c0b166ce0f5c727ad5b15c0eb08b5eb32e3d1063
mstmdev <mstmdev@gmail.com>
2024-06-03 11:31:10 +0800

    Update start_redis_cluster.sh

--- scripts/start_redis_cluster.sh
+++ scripts/start_redis_cluster.sh
@@ -5,8 +5,10 @@ docker run -d --name redis-7001 --net=host redis:latest redis-server --port 7001
 docker run -d --name redis-7002 --net=host redis:latest redis-server --port 7002 --cluster-enabled yes --appendonly yes
 docker run -d --name redis-7003 --net=host redis:latest redis-server --port 7003 --cluster-enabled yes --appendonly yes
 
-sleep 5s
+sleep 3s
 
 # create cluster
 HOST_IP=$(hostname -I | cut -d' ' -f1)
 docker run --rm redis:latest redis-cli --cluster-yes --cluster create $HOST_IP:7001 $HOST_IP:7002 $HOST_IP:7003
+
+sleep 2s
